23aa423c89e747bfa6f40c79c44cd7691c59c5e6
Frank Hunleth <fhunleth@troodon-software.com>
2016-05-04 15:38:22 -0400

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule WpaSupplicant.Mixfile do
 
   def project do
     [app: :wpa_supplicant,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
